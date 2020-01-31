4832f7fb7604c310621d986fa0e2912e595f52e2
austinoso <osorio_austin@yahoo.com>
2020-01-31 12:04:56 -0800

    austin's mock code challenge

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,33 @@
+class Passenger
+    @@all = []
+    PREMIUM_MEMBERS = []
 
+    attr_reader :name
+    attr_accessor :rides, :drivers, :total_distance
+
+    def initialize(name)
+        @name = name 
+        @rides = []
+        @drivers = []
+        @total_distance = 0
+
+        @@all << self
+    end
+
+    def add_distance(miles)
+        @total_distance += miles
+        if @total_distance > 100
+            PREMIUM_MEMBERS << self
+        end
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.premium_members
+        PREMIUM_MEMBERS
+    end
+
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -7,4 +7,23 @@ end
 
 # Put your variables here~!
 
+john = Passenger.new("john")
+james = Passenger.new("james")
+tom = Passenger.new("tom")
+
+ben = Driver.new("ben")
+frank = Driver.new("frank")
+bob = Driver.new("bob")
+
+
+ride1 = Ride.new(frank, john, 10)
+ride2 = Ride.new(frank, james, 10)
+ride3 = Ride.new(bob, tom, 10)
+ride4 = Ride.new(ben, john, 10)
+ride5 = Ride.new(bob, james, 10)
+ride6 = Ride.new(frank, tom, 10)
+ride7 = Ride.new(ben, john, 10)
+ride8 = Ride.new(bob, tom, 10)
+ride9 = Ride.new(bob, james, 10)
+
 binding.pry
