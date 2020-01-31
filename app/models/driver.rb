a5d061f1e7bfb496e69ca0261e6063b2ca064694
amnyrrd <amnyrrd@gmail.com>
2020-01-31 11:45:38 -0800

    getting there

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,28 @@
+class Driver
+    attr_accessor :name, :all
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+    end
+
+    def self.passenger_names
+        passengers = Passenger.all.uniq
+    end
+
+    def self.rides
+        rides = Ride.all
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.mileage_cap(distance)
+        over_mileage = []
+        if distance > mileage_cap
+            over_mileage << self
+        end
+    end
+end
